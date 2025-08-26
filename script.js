a284d7d9b9ed3135746264e26784538033d39f42
codewithajaydev <ajaybecse03@gmail.com>
2025-08-26 11:08:33 +0530

    pushhhhhhh

--- script.js
+++ script.js
@@ -3,7 +3,5 @@ const toggleMenu = document.querySelector('.toggle-menu');
 const navLinks = document.querySelector('.nav-links');
 
 toggleMenu.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
+  navLinks.classList.toggle('active');
 });
-
-
