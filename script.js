922ec54b4bf972c19b541a078a77b751d81c7909
codewithajaydev <ajaybecse03@gmail.com>
2025-08-26 10:09:56 +0530

    pushed

---
+++ script.js
@@ -0,0 +1,9 @@
+// Responsive Navbar Toggle
+const toggleMenu = document.querySelector('.toggle-menu');
+const navLinks = document.querySelector('.nav-links');
+
+toggleMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+
